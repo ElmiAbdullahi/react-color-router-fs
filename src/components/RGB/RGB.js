1880295d5fb5c8f14405daac060400e6306ecd00
Abdullahi Elmi <abdullahielmi147@gmail.com>
2023-01-24 17:19:34 -0800

    Not found done

--- src/components/RGB/RGB.js
+++ src/components/RGB/RGB.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import NotFound from '../NotFound/NotFound.js';
 import './RGB.css';
 
 export default function RGB() {
   const { r, g, b } = useParams();
+
+  if ((isNaN(r) || isNaN(g) || isNaN(b) || r, b, g > 255)) {
+    return <NotFound />;
+  }
+
   return (
     <main className="RGB" style={{ backgroundColor: `rgb(${r},${g},${b})` }}>
       <h2 className="rgb">{`rgb(${r},${g},${b})`}</h2>
